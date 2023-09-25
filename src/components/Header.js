b5d36694c8b94ed5db48d1aa52f99f98fab295ad
Kai <taiduong.264@gmail.com>
2023-09-25 15:51:11 +0700

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -132,12 +132,12 @@ const Header = ({ className }) => {
                                     s7.794-1.581,10.606-4.394l149.996-150C331.465,94.749,331.465,85.251,325.607,79.393z"/>
                                     </svg>
                                 </p>}
-                            <div className={`w-24 absolute z-20 right-0 top-10 ${isLogoutBadge ? "h-fit opacity-100" : "h-0 opacity-0"} overflow-hidden transition-all bg-white rounded-lg`}>
-                                <p className="flex cursor-pointer flex-row gap-2 whitespace-nowrap justify-between items-center bg-white border shadow transition-all p-2 " onClick={() => { profileDirect() }}>
+                            <div className={`w-fit border gap-2 flex flex-col px-4 py-2 absolute z-20 right-0 top-10 ${isLogoutBadge ? "h-fit opacity-100" : "h-0 opacity-0"} overflow-hidden transition-all bg-white rounded-lg`}>
+                                <p className="flex cursor-pointer flex-row gap-2 whitespace-nowrap justify-between items-center bg-white rounded-sm lg:hover:shadow-lg shadow transition-all p-2 " onClick={() => { profileDirect() }}>
                                     <span className="text-sm">Profile</span>
                                     <svg width="21" height="21" version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="info" /><g id="icons"><g id="user"><ellipse cx="12" cy="8" rx="5" ry="6" /><path d="M21.8,19.1c-0.9-1.8-2.6-3.3-4.8-4.2c-0.6-0.2-1.3-0.2-1.8,0.1c-1,0.6-2,0.9-3.2,0.9s-2.2-0.3-3.2-0.9    C8.3,14.8,7.6,14.7,7,15c-2.2,0.9-3.9,2.4-4.8,4.2C1.5,20.5,2.6,22,4.1,22h15.8C21.4,22,22.5,20.5,21.8,19.1z" /></g></g></svg>
                                 </p>
-                                <p className={`flex cursor-pointer flex-row gap-2 whitespace-nowrap justify-between items-center bg-white border shadow transition-all p-2 `}
+                                <p className={`flex cursor-pointer flex-row gap-2 whitespace-nowrap justify-between items-center bg-white rounded-sm lg:hover:shadow-lg shadow transition-all p-2 `}
                                     onClick={() => logout()} >
                                     {
                                         isLogout
